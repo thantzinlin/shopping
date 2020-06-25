5faafe4914fe8af5da3f6647fe0388d72b93a078
Thant Zin Lin <thantzinlin1995@gmail.com>
2020-06-25 23:55:04 +0630

    first day

--- src/main/java/com/tzl/Shopping/controller/LoginController.java
+++ src/main/java/com/tzl/Shopping/controller/LoginController.java
@@ -14,26 +14,31 @@ import com.tzl.Shopping.service.LoginService;
 @SessionAttributes("name")
 public class LoginController {
 	@Autowired
-    LoginService service;
+	LoginService service;
+
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String showLoginPage(ModelMap model) {
+		return "login";
+	}
 	
-	 @RequestMapping(value="/login", method = RequestMethod.GET)
-	    public String showLoginPage(ModelMap model){
-	        return "login";
-	    }
+	@RequestMapping(value = "/welcome", method = RequestMethod.GET)
+	public String showwellcomePage(ModelMap model) {
+		return "welcome";
+	}
 
-	    @RequestMapping(value="/login", method = RequestMethod.POST)
-	    public String showWelcomePage(ModelMap model, @RequestParam String name, @RequestParam String password){
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public String showWelcomePage(ModelMap model, @RequestParam String name, @RequestParam String password) {
 
-	        boolean isValidUser = service.validateUser(name, password);
+		boolean isValidUser = service.validateUser(name, password);
 
-	        if (!isValidUser) {
-	            model.put("errorMessage", "Invalid Credentials");
-	            return "login";
-	        }
+		if (!isValidUser) {
+			model.put("errorMessage", "Invalid Credentials");
+			return "login";
+		}
 
-	        model.put("name", name);
-	        model.put("password", password);
+		model.put("name", name);
+		model.put("password", password);
 
-	        return "welcome";
-	    }
+		return "welcome";
+	}
 }
